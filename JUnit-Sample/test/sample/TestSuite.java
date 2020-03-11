83aa7adfdc7b1df5f71d65cd833bd0495346812c
Jonni <Jonni@DESKTOP-0IHGBT7>
2020-03-11 15:35:04 +0100

    Commit1

---
+++ JUnit-Sample/test/sample/TestSuite.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author usuario
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({sample.UtilsTest.class, sample.VectorsTest.class})
+public class TestSuite {
+    
+}
